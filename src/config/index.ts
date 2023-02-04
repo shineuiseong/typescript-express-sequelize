816d4ce912280369fb7f88076dd390991440ff74
shineuiseong <30334829+shineuiseong@users.noreply.github.com>
2023-02-04 16:15:15 +0900

    feature(config/index.ts): env 환경변수 값 설정

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,32 @@ import path from "path";
 const env_dir = `../env/.env.${process.env.NODE_ENV || 'development'}`
 config({path: path.join(__dirname,env_dir)})
 
+export default {
+    //서버포트
+    server_port: process.env.PORT!,
+    //Log winston level
+    logs:{
+        level:process.env.LOG_LEVEL || 'silly'  as string,
+    },
+    // api 라우터 경로
+    api: {
+        prefix: '/'  as string,
+    },
+    // jwt secret
+    jwt_secret: process.env.JWT_SECRET as string,
+    mysql_config:{
+        HOST:process.env.MYSQL_HOST  as string,
+        USER:process.env.MYSQL_USER  as string,
+        PASSWORD:process.env.MYSQL_PASSWORD  as string,
+        dialect:'mysql',
+        DB:process.env.MYSQL_DATABASE  as string,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        logging:process.env.MYSQL_LOGGING
+    },
 
-
-export const {PORT} = process.env
+}
